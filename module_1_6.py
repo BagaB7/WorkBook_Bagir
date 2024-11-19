fb71e54e149c6078f992dfea49fc59285d820acd
BagaB7 <bagir7771999@gmail.com>
2024-11-19 21:28:40 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+num_of_courses_completed = 12
+num_of_hours_spent = 1.5
+name_course = 'Python'
+print(f"Курс: {name_course}, Всего задач:{num_of_courses_completed}, затрачено часов: {num_of_hours_spent}, среднее время выполнения {num_of_hours_spent / num_of_courses_completed} часа.")
+
+# Ахаха, я понял :D, в чем была моя ошибка, спасибо за помощь.
