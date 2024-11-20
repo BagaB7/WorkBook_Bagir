f9407a855c7400e87db3897bf58793af4476f7ee
BagaB7 <bagir7771999@gmail.com>
2024-11-20 20:08:44 +0500

    Add files via upload

---
+++ module_1_9.py
@@ -0,0 +1,9 @@
+example = 'Bagir'
+print(example[0])
+print(example[-1])
+print(example[2:5])
+print(example[::-1])
+print(example[1::2])
+
+example_one = 'Топинамбур'  # Так как в имени мало букв, взял длинное слово из курса
+print(example_one[1::2])
