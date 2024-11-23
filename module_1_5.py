1a96642c53b59a62843141ce8bec89cdbaab54ac
BagaB7 <bagir7771999@gmail.com>
2024-11-23 20:37:49 +0500

    module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,8 +1,6 @@
 immutable_var = 1,2,3, 'one'
 print(immutable_var)
 # print(immutable_var  .replace('1','2')) # тип данных класса tuple/кортеж  не изменяемая
-test = '1,2,3'
-# print(test .replace('1','2')) # так же код с типом int уже работает
 mutable_list = ([1, 2, 3, 'one'])
 #mutable_list [0] = 'one'
 #mutable_list [3] = 1
