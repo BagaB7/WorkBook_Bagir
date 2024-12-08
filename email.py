6184874883fada4b2128597edd3d9f016617d968
BagaB7 <bagir7771999@gmail.com>
2024-12-08 12:56:21 +0500

    email.py

--- email.py
+++ email.py
@@ -1,26 +1,23 @@
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
     a = '@'
     while True:
-        if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):   
+        if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
             if a not in recipient and sender:
                 print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
                 continue
 
-        else:
-            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-            break
-        
         if sender == recipient:
             print('Нельзя отправить письмо самому себе!')
             break
-        
+
         elif sender == 'university.help@gmail.com':
             print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
             break
-        
+
         elif sender != 'university.help@gmail.com':
             print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
             break
+
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
