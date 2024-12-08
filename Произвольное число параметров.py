f4271a31df313006e272e18dd723db2f69a19a58
BagaB7 <bagir7771999@gmail.com>
2024-12-08 17:24:24 +0500

    Произвольное число параметров.py

--- Произвольное число параметров.py
+++ Произвольное число параметров.py
@@ -2,7 +2,7 @@ def single_root_words(root_words, *other_words):
     same_words = []
 
     for i in range(len(list(other_words))):
-        if root_words.lower() in other_words[i].lower() or other_words[i].lower() in root_words.lower():
+        if root_words.lower() in other_words[i].lower() or other_words[i].upper() in root_words.upper():
             same_words.append(other_words[i])
     return (same_words)
 
