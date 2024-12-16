16141cf8c405f405e9a2e6117318a5f65a3d8826
BagaB7 <bagir7771999@gmail.com>
2024-12-16 17:41:08 +0500

    Add files via upload

--- email.py
+++ email.py
@@ -1,19 +1,23 @@
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
-
+    a = '@'
     while True:
         if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
-            if not '@' in recipient and not '@' in sender:
+            if a not in recipient and sender:
                 print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
                 continue
+
         else:
             print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
             break
+
         if sender == recipient:
             print('Нельзя отправить письмо самому себе!')
             break
+
         elif sender == 'university.help@gmail.com':
             print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
             break
+
         elif sender != 'university.help@gmail.com':
             print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
             break
