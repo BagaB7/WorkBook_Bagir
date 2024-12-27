3bf9a68ae2db0c83e9d5871f1d637468fb38fefe
BagaB7 <bagir7771999@gmail.com>
2024-12-27 19:06:29 +0500

    class_mod_1.py

--- class_mod_1.py
+++ class_mod_1.py
@@ -1,18 +1,18 @@
 class House:
 
-   def __init__(self, name, number_of_floors):
+    def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
 
-   def  go_to(self, new_flor):
-       flor_ = 0
-       if new_flor > self.number_of_floors:
-           print('Такого этажа не существует')
-       if new_flor <= self.number_of_floors:
-           flor_ = new_flor
-           for i in range(1, flor_ + 1):
-               print(i)
-               i+=1
+    def go_to(self, new_floor):
+        flor_ = 0
+        if new_floor > self.number_of_floors:
+            print(f'Такого этажа не существует  всего этажей → {self.number_of_floors}')
+        if new_floor <= self.number_of_floors:
+            flor_ = new_floor
+            for i in range(1, flor_ + 1):
+                print(f'Этаж → {i}')
+                i += 1
 
 
 h1 = House('ЖК Горский', 18)
@@ -22,4 +22,3 @@ h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 
 h2.go_to(10)
-
